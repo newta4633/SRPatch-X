33275cd357711d13870e4c0e859a27f83fc18b30
newta4633 <newta4633@gmail.com>
2025-12-17 14:53:14 +0300

    Update SignerCompat.kt

--- core/src/main/java/com/htetz/core/SignerCompat.kt
+++ core/src/main/java/com/htetz/core/SignerCompat.kt
@@ -8,6 +8,7 @@ internal object SignerCompat {
     fun findAnySign(source: DataSource): String? {
         return try {
             val result = ApkVerifier.Builder(source)
+                .setV1SigningEnabled(false)
                 .build()
                 .verify()
             result.v1SchemeSigners.firstOrNull()?.let {
